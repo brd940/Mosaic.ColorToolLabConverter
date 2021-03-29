8e995e07ca05599e7f61f1df5c3ba942b9e42ee2
Brandon Drake <brandon.r.drake@sherwin.com>
2021-03-29 13:08:55 -0400

    Add project files.

---
+++ SW.Mosaic.ColorToolLabConverter.Console/CoordinateMapperTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SW.Mosaic.ColorCalculation.Model;
+
+namespace SW.Mosaic.ColorToolLabConverter.Console
+{
+    class CoordinateMapperTest
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new CoordinateMapper();
+
+            var rawData = args[0];
+
+            var illuminantObserver = args[1];
+
+            System.Console.WriteLine($"Calling 'ConvertCieLabToJson' in 'CoordinateMapper.DLL'\n");
+            System.Console.WriteLine($"With Arguments:\nRaw Data: {rawData}\nIlluminant Observer: {illuminantObserver}\n\n");
+
+            var result = calculator.ConvertCIELabToJson(rawData, illuminantObserver);
+            
+            System.Console.WriteLine($"Result as JSON:\n{result}");
+        }
+    }
+}
---
+++ SW.Mosaic.ColorToolLabConverter/CoordinateMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+using SW.Mosaic.ColorCalculation;
+using SW.Mosaic.ColorCalculation.Model;
+namespace SW.Mosaic.ColorToolLabConverter
+{
+    public class CoordinateMapper
+    {
+        private CIELab MapRawToCieLab(string rawData, string illuminantObserver)
+        {
+            var spectrumToXyzConverter = new SpectrumToXYZConverter();
+            var xyzToLabConverter = new ColorSpaceHelper(spectrumToXyzConverter);
+            var spectrumToLabConverter = new SpectrumToLabConverter(spectrumToXyzConverter, xyzToLabConverter);
+            var coordinateMapper = new ColorCalculation.CoordinateMapper(spectrumToLabConverter);
+
+            return coordinateMapper.MapRawToCieLab(rawData, illuminantObserver);
+        }
+
+        public string ConvertCIELabToJson(string rawData, string illuminantObserver)
+        {
+            Console.WriteLine("Calling 'MapRawToCieLab' to retrieve the LAB conversion as a CieLab object\n");
+
+            var result = MapRawToCieLab(rawData, illuminantObserver);
+
+            Console.WriteLine("Serializing the Object to JSON\n\n");
+
+            return JsonSerializer.Serialize(result);
+        }
+
+    }
+}
